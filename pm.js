a87248b7216f487a69fbb69a738033770562183e
norma-forson <norma.forson@ashesi.edu.gh>
2022-04-27 15:44:26 +0000

    completed project

--- pm.js
+++ pm.js
@@ -2,7 +2,7 @@ function myFunction(){
     var un =document.forms["myForm"]["email"].value;
     var pw =document.forms["myForm"]["YearGroup"].value;
     if((un.includes("@ashesi.edu.gh") && pw>=2022 && pw<=2025)){
-        window.location.href="index.html";
+        window.location.href="pages.html";
     }
     else{
         alert("Invalid Username and password");
